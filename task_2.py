ef0f3fe3d6c6bba69ec60ee46e5d396af16437e7
Grover_py <Nebu23z@yandex.ru>
2021-02-21 12:06:48 +0300

    fix task_2

--- task_2.py
+++ task_2.py
@@ -13,11 +13,10 @@ print(is_year_leap(2021))
 
 def is_year_leap_2(y):
     my_list = [el for el in range(0,10000,4)]
-    while True:
-        if y in my_list:
-            return True
-        else:
-            return False
+    if y in my_list:
+        return True
+    else:
+        return False
 
 print(is_year_leap_2(2020))
 print(is_year_leap_2(2021))
